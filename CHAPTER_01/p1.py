e6e59e654c3eda876c8f840f411f335ee4a1bac8
Jae-Young98 <ygy0102@gmail.com>
2022-01-16 20:39:46 +0900

    p1.py
    Day 1

---
+++ CHAPTER_01/p1.py
@@ -0,0 +1,23 @@
+# Const
+PI = 3.14
+GRAVITY = 9.8
+
+print(PI)
+
+#Var, Print
+a = 100
+b = 3.14
+c = 'k'
+d = 'Hello World!'
+
+print('[결과 출력]')
+print('a 변수의 값은?', a)
+print('b 변수의 값은?', b)
+print('c 변수의 값은?', c)
+print('d 변수의 값은?', d)
+print(a, b, c, d)
+
+# Multiple assignment
+a = b = c = d = e = 100
+print('[결과출력]')
+print(a, b, c, d, e)
