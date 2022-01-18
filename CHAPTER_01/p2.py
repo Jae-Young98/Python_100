4c6fd8d6fa05e9ab24fe3a978aab0acca4d62a7b
Jae-Young98 <ygy0102@gmail.com>
2022-01-18 19:07:18 +0900

    string, id

---
+++ CHAPTER_01/p2.py
@@ -0,0 +1,9 @@
+# String
+sample_txt = '샘플 텍스트."큰따옴표" 출력하기.'
+print(sample_txt)
+
+sample_txt2 = '샘플 텍스트. "따옴표 안에 \'따옴표\'" 출력하기. (c언어랑 동일하다.)'
+print(sample_txt2)
+
+sample_txt3 = "샘플 텍스트. \"따옴표 안에 '따옴표'\" 출력하기. (가독성 좋게 바꾸기)"
+print(sample_txt3)
---
+++ CHAPTER_01/p3.py
@@ -0,0 +1,3 @@
+# Variable_id
+a = [1, 2, 3]
+print(a, type(a), id(a))  # id는 고유 메모리 주소를 나타낸다
